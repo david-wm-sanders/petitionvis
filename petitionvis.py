f625bbc480832f853c55220b720f58d24d5aa7a2
David Sanders <david.wm.sanders@gmail.com>
2016-04-19 03:50:40 +0100

    Colour the constituencies

--- petitionvis.py
+++ petitionvis.py
@@ -3,12 +3,10 @@ import sys
 import json
 from operator import methodcaller
 from pathlib import Path
-import shapefile
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
 from mpl_toolkits.basemap import Basemap
-from matplotlib.collections import LineCollection
+from matplotlib.patches import Polygon
 
 if len(sys.argv) != 2:
     print("Usage: petitionvis.py <petition number>")
@@ -35,6 +33,15 @@ print("Action:", action)
 print("Author:", author)
 print("Signum:", signum)
 
+# Create a dictionary of ons_code:signature_count
+sigcount_by_constcode = {c["ons_code"]: c for c in petition["signatures_by_constituency"]}
+
+# Find the highest number of signatures
+sigs_by_constituency = petition["signatures_by_constituency"]
+sigs_by_constituency.sort(key=methodcaller("get", "signature_count"), reverse=True)
+high_sigcount = sigs_by_constituency[0]["signature_count"]
+high_sigcount = high_sigcount + high_sigcount * 0.25
+
 # Make map
 print("Making map:")
 fig = plt.figure(figsize=(12, 16))
@@ -43,13 +50,16 @@ print("Setting up map...")
 plt.title("{0}, {1}, {2}\nVotes by Constituency".format(petition_id, action, signum))
 m = Basemap(resolution="h", projection="merc",
             llcrnrlon=-10.6, llcrnrlat=49.9, urcrnrlon=1.9, urcrnrlat=59.4, ax=ax)
-m.drawcoastlines(linewidth=0.75)
-m.drawcountries(linewidth=0.5)
+m.drawcoastlines(linewidth=0.5)
+m.drawcountries(linewidth=0.25)
 print("Loading shapefile...")
-m.readshapefile("westminster_const_region", "constituencies", linewidth=0.25)
-lon, lat = -3.034, 51.8246
-x, y = m(lon, lat)
-ax.plot(x, y, "ro", markersize=3)
+m.readshapefile("westminster_const_region", "constituencies", linewidth=0.25, drawbounds=False)
+print("Processing shapefile...")
+for info, shape in zip(m.constituencies_info, m.constituencies):
+    ons_code = info["CODE"]
+    sigcount = sigcount_by_constcode[ons_code]["signature_count"]
+    p = Polygon(np.array(shape), linewidth=0.1, ec="k", fc=str(1 - sigcount / high_sigcount))
+    ax.add_patch(p)
 
 print("Saving map...")
 plt.savefig("fig/{0}_{1}_map.png".format(petition_id, signum), dpi=300)
